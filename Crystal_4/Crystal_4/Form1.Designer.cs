0a0c8c5ed3a8e14477350eb2fd4c28e546ecae37
Edd <edcruces99@gmail.com>
2021-07-08 22:22:57 -0500

    Carpeta Crystal_4
    Carpeta Crystal_4 que contiene un ejemplo de como generar un reporte en Crystal Reports

---
+++ Crystal_4/Crystal_4/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Crystal_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DataSet1 = new Crystal_4.DataSet1();
+            this.REPORTE_USUARIOSBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.REPORTE_USUARIOSTableAdapter = new Crystal_4.DataSet1TableAdapters.REPORTE_USUARIOSTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.REPORTE_USUARIOSBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.REPORTE_USUARIOSBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Crystal_4.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(724, 416);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // DataSet1
+            // 
+            this.DataSet1.DataSetName = "DataSet1";
+            this.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // REPORTE_USUARIOSBindingSource
+            // 
+            this.REPORTE_USUARIOSBindingSource.DataMember = "REPORTE_USUARIOS";
+            this.REPORTE_USUARIOSBindingSource.DataSource = this.DataSet1;
+            // 
+            // REPORTE_USUARIOSTableAdapter
+            // 
+            this.REPORTE_USUARIOSTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 416);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.REPORTE_USUARIOSBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource REPORTE_USUARIOSBindingSource;
+        private DataSet1 DataSet1;
+        private DataSet1TableAdapters.REPORTE_USUARIOSTableAdapter REPORTE_USUARIOSTableAdapter;
+    }
+}
+
---
+++ Crystal_4/Crystal_4/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crystal_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Este reporte se construyó con el tutorial: https://youtu.be/MPKqjffs1d8
+            //Video: Reportes en C# .Net con VS 2019 (3-3) Diseñando nuestro reporte
+            //Autor: Juan Carlos Arcila Díaz (autor del video)
+
+            //Paso 1. Botón derecho en Crystal_4 > Add > New Item > Data > Dataset
+            //Paso 2. Seleccionar el Dataset > Add > Table Adapter
+            //Paso 3. Configurar Table Adapter (Con el query a utilizar)
+            //Paso 4. Botón derecho en Crystal_4 > Add > New Item > Reporting > Report (sin wizard)
+            //Paso 5. Seleccionar Form1 y agregar componente ReportViewer
+            
+            // TODO: This line of code loads data into the 'DataSet1.REPORTE_USUARIOS' table. You can move, or remove it, as needed.
+            this.REPORTE_USUARIOSTableAdapter.Fill(this.DataSet1.REPORTE_USUARIOS);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
